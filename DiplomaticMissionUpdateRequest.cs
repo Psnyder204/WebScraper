5c4a0572e3a2d36bcf4edf8d75daa973e00996e8
Patrick Snyder <117422938+Psnyder204@users.noreply.github.com>
2023-02-24 10:45:57 -0800

    Update DiplomaticMissionUpdateRequest.cs

--- DiplomaticMissionUpdateRequest.cs
+++ DiplomaticMissionUpdateRequest.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Sabio.Models.Requests.DiplomaticMissions
+namespace Models.Requests.DiplomaticMissions
 {
     public class DiplomaticMissionUpdateRequest : DiplomaticMissionAddRequest, IModelIdentifier
     {
